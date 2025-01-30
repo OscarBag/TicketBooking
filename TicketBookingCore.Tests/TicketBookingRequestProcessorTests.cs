2288d56d0b0a4e8773e2307c2825b2126847d08f
OscarBag <bagler.oscar@gmail.com>
2025-01-30 09:28:57 +0100

    Add project files.

---
+++ TicketBookingCore.Tests/TicketBookingRequestProcessorTests.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TicketBookingCore.Tests
+{
+    public class TicketBookingRequestProcessorTests
+    {
+        public readonly TicketBookingRequestProcessor _processor;
+        public TicketBookingRequestProcessorTests()
+        {
+            _processor = new TicketBookingRequestProcessor();
+        }
+        
+
+        [Fact]
+        public void ShouldReturnTicketBookingResultWithRequestValues()
+        {
+
+            var request = new TicketBookingRequest
+            {
+                FirstName = "Oscar",
+                LastName = "Bagler",
+                Email = "oscar@email.com"
+            };
+
+            //Act
+            TicketBookingResponse response = _processor.Book(request);
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.Equal(request.FirstName, response.FirstName);
+            Assert.Equal(request.LastName, response.LastName);
+            Assert.Equal(request.Email, request.Email);
+        }
+        [Fact]
+        public void ShouldThrowExceptionIfRequestIsNull()
+        {
+            //Act
+            var exception = Assert.Throws<ArgumentNullException>(() => _processor.Book(null));
+
+            //Assert
+            Assert.Equal("request", exception.ParamName);    
+        }
+    }
+}
